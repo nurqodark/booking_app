bf56f0d04f8d5ce906c3be8bff65baad73feb1ba
nurqodark <brigudavr1@gmail.com>
2023-01-12 02:01:29 +0700

    button

--- lib/presentation/widget/disable_button.dart
+++ lib/presentation/widget/disable_button.dart
@@ -1,83 +1,84 @@
-
 import 'package:booking/config/config.dart';
 import 'package:flutter/material.dart';
 
-class PrimaryButton extends StatelessWidget {
-  const PrimaryButton({Key? key,
-    this.type = PrimaryButtonType.type1,
+class DisableButton extends StatelessWidget {
+  const DisableButton({Key? key,
+
+    this.type = DisableButtonType.type1,
     required this.onPressed,
     required this.text,
     this.width = 78,
+
   }) : super(key: key);
 
-  final PrimaryButtonType type;
+  final DisableButtonType type;
   final VoidCallback? onPressed;
   final String text;
   final double height = 24;
   final double width;
 
+
   @override
   Widget build(BuildContext context) {
-    return type == PrimaryButtonType.type1 ? _type1() : type == PrimaryButtonType.type2 ? _type2() : _type3();
+    return type == DisableButtonType.type1 ? _type1() : type == DisableButtonType.type2 ? _type2() : _type3();
   }
+
   Widget _type1(){
     return SizedBox(
-      width: width,
+      width: 78,
       height: height,
       child: ElevatedButton(
-        style: ElevatedButton.styleFrom(
-          backgroundColor: AppColor.ink01,
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(4),
-          )
-        ),
         onPressed: onPressed,
-          child: Text(text,
-            style: AppFont.componentSmall.copyWith(color: AppColor.ink06),
-          ),
+        style: ElevatedButton.styleFrom(
+            backgroundColor: AppColor.ink06,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(4),
+            )
+        ),
+        child: Text(text,
+            style: AppFont.componentSmall.copyWith(color: AppColor.ink01,)),
       ),
     );
   }
   Widget _type2(){
     return SizedBox(
-      width: width,
+      width: 96,
       height: 32,
       child: ElevatedButton(
+        onPressed: onPressed,
         style: ElevatedButton.styleFrom(
-            backgroundColor: AppColor.ink01,
+            backgroundColor: AppColor.ink06,
             shape: RoundedRectangleBorder(
               borderRadius: BorderRadius.circular(4),
             )
         ),
-        onPressed: onPressed,
         child: Text(text,
-          style: AppFont.componentMediumBold.copyWith(color: AppColor.ink06),
-        ),
+            style: AppFont.componentMediumBold.copyWith(color: AppColor.ink01,)),
       ),
     );
   }
   Widget _type3(){
     return SizedBox(
-      width: width,
+      width: 111,
       height: 40,
       child: ElevatedButton(
+        onPressed: onPressed,
         style: ElevatedButton.styleFrom(
-            backgroundColor: AppColor.ink01,
+            backgroundColor: AppColor.ink06,
             shape: RoundedRectangleBorder(
               borderRadius: BorderRadius.circular(4),
             )
         ),
-        onPressed: onPressed,
         child: Text(text,
-          style: AppFont.componentLarge.copyWith(color: AppColor.ink06),
-        ),
+            style: AppFont.componentLarge.copyWith(color: AppColor.ink01,)),
       ),
     );
   }
+
 }
-enum PrimaryButtonType{
+
+enum DisableButtonType{
   type1,
   type2,
   type3
 }
-
--- lib/presentation/widget/secondary_button.dart
+++ lib/presentation/widget/secondary_button.dart
@@ -1,83 +1,87 @@
-
 import 'package:booking/config/config.dart';
 import 'package:flutter/material.dart';
 
-class PrimaryButton extends StatelessWidget {
-  const PrimaryButton({Key? key,
-    this.type = PrimaryButtonType.type1,
+class SecondaryButton extends StatelessWidget {
+  const SecondaryButton({Key? key,
+
+    this.type = SecondaryButtonType.type1,
     required this.onPressed,
     required this.text,
     this.width = 78,
+
+
+
+
   }) : super(key: key);
 
-  final PrimaryButtonType type;
+  final SecondaryButtonType type;
   final VoidCallback? onPressed;
   final String text;
   final double height = 24;
   final double width;
 
+
   @override
   Widget build(BuildContext context) {
-    return type == PrimaryButtonType.type1 ? _type1() : type == PrimaryButtonType.type2 ? _type2() : _type3();
+    return type == SecondaryButtonType.type1 ? _type1() : type == SecondaryButtonType.type2 ? _type2() : _type3();
   }
+
   Widget _type1(){
     return SizedBox(
-      width: width,
+      width: 78,
       height: height,
       child: ElevatedButton(
+        onPressed: onPressed,
         style: ElevatedButton.styleFrom(
-          backgroundColor: AppColor.ink01,
+          backgroundColor: AppColor.ink06,
           shape: RoundedRectangleBorder(
             borderRadius: BorderRadius.circular(4),
           )
         ),
-        onPressed: onPressed,
-          child: Text(text,
-            style: AppFont.componentSmall.copyWith(color: AppColor.ink06),
-          ),
+        child: Text(text,
+          style: AppFont.componentSmall.copyWith(color: AppColor.ink01,)),
       ),
     );
   }
   Widget _type2(){
     return SizedBox(
-      width: width,
+      width: 96,
       height: 32,
       child: ElevatedButton(
+        onPressed: onPressed,
         style: ElevatedButton.styleFrom(
-            backgroundColor: AppColor.ink01,
+            backgroundColor: AppColor.ink06,
             shape: RoundedRectangleBorder(
               borderRadius: BorderRadius.circular(4),
             )
         ),
-        onPressed: onPressed,
         child: Text(text,
-          style: AppFont.componentMediumBold.copyWith(color: AppColor.ink06),
-        ),
+            style: AppFont.componentMediumBold.copyWith(color: AppColor.ink01,)),
       ),
     );
   }
   Widget _type3(){
     return SizedBox(
-      width: width,
+      width: 111,
       height: 40,
       child: ElevatedButton(
+        onPressed: onPressed,
         style: ElevatedButton.styleFrom(
-            backgroundColor: AppColor.ink01,
+            backgroundColor: AppColor.ink06,
             shape: RoundedRectangleBorder(
               borderRadius: BorderRadius.circular(4),
             )
         ),
-        onPressed: onPressed,
         child: Text(text,
-          style: AppFont.componentLarge.copyWith(color: AppColor.ink06),
-        ),
+            style: AppFont.componentLarge.copyWith(color: AppColor.ink01,)),
       ),
     );
   }
+
 }
-enum PrimaryButtonType{
+
+enum SecondaryButtonType{
   type1,
   type2,
   type3
 }
-
